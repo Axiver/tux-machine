3335d27fbe3b3e58fb01adaf22d1bf0f9a7ea5b9
Garlicvideos <Garlicvideos@gmail.com>
2019-03-05 21:02:09 +0800

    works with kal-explorer now
    kal-explorer doesn't have a transaction socket yet so it only visualises blocks for now

--- src/dash-machine.js
+++ src/dash-machine.js
@@ -858,7 +858,10 @@ DashMachine.prototype.audio = function (_this) {
                 if (pool[i][j].paused || pool[i][j].ended) {
                     pool[i][j].playbackRate = _this.RandRange(0.9, 1.1);
                     pool[i][j].volume = (vol || 0.5) * audioBias;
-                    pool[i][j].play();
+                    let playPromise = pool[i][j].play();
+                    if (playPromise !== null) {
+                    	playPromise.catch(() => { /* discard runtime error */ });
+                    }
                     break;
                 }
             }
@@ -916,10 +919,10 @@ DashMachine.prototype.net = function (_this) {
     function pingBestBlock() {
         // Wait for enough inactivity
         if (lastConnected ? (new Date() - lastConnected > 30000) : true) {
-            get("status?q=getBestBlockHash", function (res1) {
+            get("status?q=getInfo", function (res1) {
                 // first ping?
                 if (!_this.bestBlock)
-                    get("block/" + res1.bestblockhash, function (res) {
+                    get("block/" + res1.lastblockhash, function (res) {
                         if (res && !_this.bestBlock) {
                             getBlockTX(res);
                         }
@@ -932,17 +935,17 @@ DashMachine.prototype.net = function (_this) {
 
     function getBlockTX(block) {
         block.txAmount = [];
-        var maxTx = Math.min(block.tx.length, 30);
+        var maxTx = Math.min(block.txs.length, 30);
         var cbCount = 0;
         var outCount = 0;
         // request amounts for each TXO based on the address
         for (var i = 0; i < maxTx; i++) {
-            get("tx/" + block.tx[i], function (data) {
+            get("tx/" + block.txs[i].txid, function (data) {
                 cbCount++;
                 if (data && !_this.bestBlock) {
-                    for (var i = 0; i < data.vout.length; i++) {
+                    for (var i = 0; i < data.addresses_out.length; i++) {
                         outCount++;
-                        block.txAmount[outCount - 1] = data.vout[i].value;
+                        block.txAmount[outCount - 1] = data.addresses_out[i].value;
                         if (block.txAmount.length >= 100) {
                             _this.bestBlock = block;
                             break;
@@ -973,15 +976,15 @@ DashMachine.prototype.net = function (_this) {
             socket.emit('subscribe', 'inv');
         });
         socket.on('block', function (data) {
-            get("block/" + data, function (res) {
+            get("block/" + data.hash, function (res) {
                 _this.AddBlock(res);
             });
         });
         socket.on('tx', function (data) {
             setNetConnect();
-            for (var i = 0; i < data.vout.length; i++)
-                for (var prop in data.vout[i]) {
-                    _this.Objects.AddTX(data.vout[i][prop] / 10000000);
+            for (var i = 0; i < data.addresses_out.length; i++)
+                for (var prop in data.addresses_out[i]) {
+                    _this.Objects.AddTX(data.addresses_out[i][prop] / 10000000);
                     break;
                 }
         });
@@ -989,7 +992,7 @@ DashMachine.prototype.net = function (_this) {
 
     // xhr
     function get(url, cb) {
-        url = _this.options.apiURL + 'insight-tux-api/' + url;
+        url = _this.options.apiURL + "api/" + url;
         var xhr = new XMLHttpRequest();
         if ("withCredentials" in xhr) {
             xhr.open('GET', url, true); // Browsers
